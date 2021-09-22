ec23e1218c12efe2b170961d3092d0fe674a436b
samanthakazla <samanthakazla@gmail.com>
2021-09-22 07:36:59 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -4,4 +4,23 @@ const server = express();
 
 server.use(express.json());
 
+server.use('/api/accounts', accountsRouter)
+server.use('*', notFound)
+server.use(errorHandling)
+
+const errorHandling = (err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message,
+})
+}
+
+
+const notFound = (req, res, next) => {
+    res.status(404).json({
+        message: 'Not found'
+})
+}
+
+
 module.exports = server;
